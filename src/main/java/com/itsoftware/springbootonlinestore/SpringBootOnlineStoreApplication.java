fb11a152c44eec2a4e8467af9f881bdb4655f29d
Arkadiusz <arek1990cda@gmail.com>
2022-01-24 16:14:15 +0100

    initialization spring boot project

---
+++ src/main/java/com/itsoftware/springbootonlinestore/SpringBootOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.itsoftware.springbootonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsoftware/springbootonlinestore/SpringBootOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsoftware.springbootonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
